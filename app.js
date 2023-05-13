65beae48fd3ecc2aeee81671c4303bd7f5f367b8
iamharshdabas <iamharshdabas@gmail.com>
2023-05-13 16:30:46 +0530

    ADD: post item list view
    UP (app.js): post request

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const today = new Date().toLocaleDateString('en-us', { weekday: 'long', month: 'long', day: 'numeric' });
+let newItem;
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (_, res) => {
-  res.render('index', { today: today });
+  res.render('index', {
+    today: today,
+    newItem: newItem
+  });
 });
 
-app.post('/', (req, _) => {
-  console.log(req.body.newItem);
-})
+app.post('/', (req, res) => {
+  newItem = req.body.newItem;
+  res.redirect('/');
+});
 
 app.listen(6969, () => console.log('Server app.js listening on port 6969'));
