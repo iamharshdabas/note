434f70d4cf9e043ac88e7eb5441a0c4f9b71eb13
iamharshdabas <iamharshdabas@gmail.com>
2023-05-13 15:51:30 +0530

    UP: app.js date format

--- app.js
+++ app.js
@@ -2,38 +2,12 @@ const express = require('express');
 // const bodyParser = require('body-parser');
 const app = express();
 
-const today = new Date();
-const todayDay = today.getDay();
-let dayName
-
-switch (todayDay) {
-  case 0:
-    dayName = 'Sunday';
-    break;
-  case 1:
-    dayName = 'Monday';
-    break;
-  case 2:
-    dayName = 'Tuesday';
-    break;
-  case 3:
-    dayName = 'Wednesday';
-    break;
-  case 4:
-    dayName = 'Thursday';
-    break;
-  case 5:
-    dayName = 'Friday';
-    break;
-  case 6:
-    dayName = 'Saturday';
-    break;
-}
+const today = new Date().toLocaleDateString('en-us', { weekday:"long", month:"long", day:"numeric"});
 
 app.set('view engine', 'ejs');
 
 app.get('/', (_, res) => {
-  res.render('index', { dayName: dayName });
+  res.render('index', { today: today});
 });
 
 app.listen(6969, () => console.log('Server app.js listening on port 6969'));
