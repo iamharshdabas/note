e3b3cedafe52fbe4a9589120668d151484c960cb
iamharshdabas <iamharshdabas@gmail.com>
2023-05-13 12:21:21 +0530

    ADD: index.ejs weekday

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const today = new Date();
+const todayDay = today.getDay();
+let dayName
+
+if (todayDay === 0 || todayDay === 6) {
+  dayName = 'its weekend'
+} else {
+  dayName = 'its weekday'
+}
+
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Working')
+  res.render('index', { dayName: dayName });
 });
 
 app.listen(6969, () => console.log('Server app.js listening on port 6969'));
