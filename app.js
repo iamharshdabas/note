b6836ddea6231c3d11c4af258b0eda9ae0396993
iamharshdabas <iamharshdabas@gmail.com>
2023-05-13 16:17:11 +0530

    ADD: item form
    ADD: app.js post request

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const app = express();
 
-const today = new Date().toLocaleDateString('en-us', { weekday:"long", month:"long", day:"numeric"});
+const today = new Date().toLocaleDateString('en-us', { weekday: 'long', month: 'long', day: 'numeric' });
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (_, res) => {
-  res.render('index', { today: today});
+  res.render('index', { today: today });
 });
+
+app.post('/', (req, _) => {
+  console.log(req.body.newItem);
+})
 
 app.listen(6969, () => console.log('Server app.js listening on port 6969'));
